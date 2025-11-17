28f5c614582218c51108512e242e3102544a8870
Yerayzl1 <yerayzafra1@gmail.com>
2025-11-17 09:24:16 +0100

    Tipos básicos

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ class Program
 {
   static void Main()
   {
-    WriteLine("Hello, World!");
+    // Declaración explicíta
+    int quantity = 5;
+    string message = "Hello world";
+    decimal price = 19.99m;
+    WriteLine($" Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+
+    // Declaración con var
+    var gretting = "Hola";
+    var porcentage = 20.00m;
+    WriteLine($" Saludo: {gretting}, Porcentaje: {porcentage}");
+
   }
 }
